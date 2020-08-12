bf79fb2ef2ac79d406357854d32824e21abe3c76
VicThorIH <victoribarrah2.gmail.com>
2020-08-12 11:42:12 -0600

    Metodos chidos añadidos

--- Gato/src/Gato.java
+++ Gato/src/Gato.java
@@ -9,13 +9,184 @@ public class Gato {
 								{"-","-","-","-","-"},
 								{"7","|","8","|","9"} };
 		
+		String turno = "J1";
 		
+		
+		Imprimir_Gato(matriz);
+		turno = Reemplazar_Gato(9, matriz, turno);
+		Imprimir_Gato(matriz);
+		turno = Reemplazar_Gato(7, matriz, turno);
+		Imprimir_Gato(matriz);
+		turno = Reemplazar_Gato(5, matriz, turno);
 		Imprimir_Gato(matriz);
 		
 		
 		
 		
+	}
+	
+	
+	public static String Reemplazar_Gato(int n, String[][] m, String t) {
 		
+		switch (n) {
+		case 1:
+			if(m[0][0] != "1"){
+				System.err.println("Escoje otro numero");
+				return t;
+			}else {
+				if (t == "J1") {
+					m[0][0] = "x";
+					return "J2";
+				}else {
+					m[0][0] = "o";
+					return "J1";
+				}
+				
+			}
+			
+			
+		case 2:
+			
+			if(m[0][2] != "2"){
+				System.err.println("Escoje otro numero");
+				return t;
+			}else {
+				if (t == "J1") {
+					m[0][2] = "x";
+					return "J2";
+				}else {
+					m[0][2] = "o";
+					return "J1";
+				}
+				
+			}
+			
+			
+		case 3:
+			
+			if(m[0][4] != "3"){
+				System.err.println("Escoje otro numero");
+				return t;
+			}else {
+				if (t == "J1") {
+					m[0][4] = "x";
+					return "J2";
+				}else {
+					m[0][4] = "o";
+					return "J1";
+				}
+				
+			}
+			
+			
+		case 4:
+			
+			if(m[2][0] != "4"){
+				System.err.println("Escoje otro numero");
+				return t;
+			}else {
+				if (t == "J1") {
+					m[2][0] = "x";
+					return "J2";
+				}else {
+					m[2][0] = "o";
+					return "J1";
+				}
+				
+			}
+			
+			
+		case 5:
+			
+			if(m[2][2] != "5"){
+				System.err.println("Escoje otro numero");
+				return t;
+			}else {
+				if (t == "J1") {
+					m[2][2] = "x";
+					return "J2";
+				}else {
+					m[2][2] = "o";
+					return "J1";
+				}
+				
+			}
+			
+			
+		case 6:
+			
+			if(m[2][4] != "6"){
+				System.err.println("Escoje otro numero");
+				return t;
+			}else {
+				if (t == "J1") {
+					m[2][4] = "x";
+					return "J2";
+				}else {
+					m[2][4] = "o";
+					return "J1";
+				}
+				
+			}
+			
+			
+		case 7:
+			
+			if(m[4][0] != "7"){
+				System.err.println("Escoje otro numero");
+				return t;
+			}else {
+				if (t == "J1") {
+					m[4][0] = "x";
+					return "J2";
+				}else {
+					m[4][0] = "o";
+					return "J1";
+				}
+				
+			}
+			
+			
+		case 8:
+			
+			if(m[4][2] != "8"){
+				System.err.println("Escoje otro numero");
+				return t;
+			}else {
+				if (t == "J1") {
+					m[4][2] = "x";
+					return "J2";
+				}else {
+					m[4][2] = "o";
+					return "J1";
+				}
+				
+			}
+			
+			
+		case 9:
+			
+			if(m[4][4] != "9"){
+				System.err.println("Escoje otro numero");
+				return t;
+			}else {
+				if (t == "J1") {
+					m[4][4] = "x";
+					return "J2";
+				}else {
+					m[4][4] = "o";
+					return "J1";
+				}
+				
+			}
+			
+			
+			
+		default:
+			System.out.println("Escoje un número válido");
+			return t;
+			
+		}
 	}
 
 	public static void Imprimir_Gato(String[][] matriz) {
@@ -25,6 +196,9 @@ public class Gato {
 			}
 			System.out.println();
 		}
+		System.out.println();
+		System.out.println();
+		System.out.println();
 	}
 
 }
